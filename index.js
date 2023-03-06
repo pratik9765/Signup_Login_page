4d71fe70ddf859db61ccd5717dec6fab40cfba92
pratik9765 <pratikbokde9765@gmail.com>
2023-03-06 21:19:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// let signup = document.querySelector("[signup]");
+// let login = document.querySelector("[login]");
+
+// let signupButton = document.querySelector("[signupBtn]");
+// let loginButton = document.querySelector("[loginBtn]");
+
+// signupButton.addEventListener("click", makeLoginInvisible);
+// loginButton.addEventListener("click", makeSignupInvisible);
+
+// function makeLoginInvisible(){
+
+//     login.classList.remove("invisible");
+//     login.classList.add("active");
+//     signup.classList.add("active");
+//     signup.classList.remove("invisible");
+    
+
+// }
+
+
+// function makeSignupInvisible(){
+//   signup.classList.remove("active");
+//   signup.classList.add("invisible");
+//   login.classList.remove("invisible");
+//   login.classList.add("active");
+  
+        
+
+// }
